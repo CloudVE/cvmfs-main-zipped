e9ae928b79a613ceee5cf60ec5772f96cb07df5e
almahmoud <amahmou4@jhu.edu>
2019-09-25 14:24:57 -0400

    Remove /cvmfs/main/ from shed tools dir

--- modify.sh
+++ modify.sh
@@ -4,7 +4,9 @@ rm -rf ./working/
 mkdir ./working/
 cp -r /cvmfs/main/config ./working/
 cp -r /cvmfs/main/tool_data ./working/
-find /cvmfs/main/shed_tools -name '*.xml' -exec cp --parents \{\} ./working/ \;
+find /cvmfs/main/shed_tools -type f -name '*.xml' -exec cp --parents \{\} ./working/ \;
+mv ./working/cvmfs/main/shed_tools ./working/shed_tools
+rm -rf ./working/cvmfs/main/
 find ./working/ -type f -exec sed -i.cvmfsoriginal -e 's%cvmfs/main.galaxyproject.org%cvmfs/zipped%g' {} \;
 rm -rf ./previous/
 mv ./current/ ./previous/
