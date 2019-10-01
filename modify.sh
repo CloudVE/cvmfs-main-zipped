42bbf39d49af958a00cc217269cec8491d341540
almahmoud <amahmou4@jhu.edu>
2019-10-01 10:46:31 -0400

    Copying all shed_tools w/out .hg and test-data

--- modify.sh
+++ modify.sh
@@ -4,7 +4,7 @@ rm -rf ./working/
 mkdir ./working/
 cp -r /cvmfs/main/config ./working/
 cp -r /cvmfs/main/tool_data ./working/
-find /cvmfs/main/shed_tools -type f -name '*.xml' -exec cp --parents \{\} ./working/ \;
+find /cvmfs/main/shed_tools -type f -not -path "/cvmfs/main/shed_tools/*/{.hg,test-data}" -exec cp --parents \{\} ./working/ \;
 mv ./working/cvmfs/main/shed_tools ./working/shed_tools
 rm -rf ./working/cvmfs/main/
 find ./working/ -type f -exec sed -i.cvmfsoriginal -e 's%cvmfs/main.galaxyproject.org%cvmfs/zipped%g' {} \;
