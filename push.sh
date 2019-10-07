a4277523fe5c590966158f7728ea0fc304a4616a
almahmoud <amahmou4@jhu.edu>
2019-10-07 18:52:39 -0400

    Specifying adds

--- push.sh
+++ push.sh
@@ -3,8 +3,10 @@
 git config --global user.email "travis@travis-ci.org"p
 git config --global user.name "Travis CI"
 git add .
+git add ./current
+git add ./previous
 git commit -m "Travis update: $(date) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]" > ./current/commit.log
-git add .
+git add ./current
 git commit -m "Commit Log of Travis update: $(date) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 git remote add origin-token https://almahmoud:${GH_TOKEN}@github.com/CloudVE/cvmfs-main-zipped.git
 git push --quiet origin-token master > /dev/null 2>&1
