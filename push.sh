10ebbe166feb1ed26528e389bbda4066c141fff1
almahmoud <amahmou4@jhu.edu>
2019-10-07 18:06:45 -0400

    Removing travis wait + Adding commit for commit log

--- push.sh
+++ push.sh
@@ -4,6 +4,8 @@ git config --global user.email "travis@travis-ci.org"p
 git config --global user.name "Travis CI"
 git checkout master
 git add .
-git commit -m "Travis update: $(date) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+git commit -m "Travis update: $(date) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]" > ./current/commit.log
+git add .
+git commit -m "Commit Log of Travis update: $(date) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 git remote add origin-token https://almahmoud:${GH_TOKEN}@github.com/CloudVE/cvmfs-main-zipped.git
 git push --quiet origin-token master > /dev/null 2>&1
