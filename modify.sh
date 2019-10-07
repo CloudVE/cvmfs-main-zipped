ae9b7167b54ddd2b6935ea87c1184df77bb2ab3c
almahmoud <amahmou4@jhu.edu>
2019-10-07 17:23:40 -0400

    Removing Pull

--- modify.sh
+++ modify.sh
@@ -6,7 +6,7 @@ cp -r /cvmfs/main/config ./working/
 cp -r /cvmfs/main/tool_data ./working/
 find /cvmfs/main/shed_tools -type f -not -path "*/test-data/*" -not -path "*/.hg/*" -exec cp --parents \{\} ./working/ \;
 mv ./working/cvmfs/main/shed_tools ./working/shed_tools
-rm -rf ./working/cvmfs/main/
+rm -rf ./working/cvmfs/
 find ./working/ -type f -exec sed -i.cvmfsoriginal -e 's%cvmfs/main.galaxyproject.org%cvmfs/zipped%g' {} \;
 rm -rf ./previous/
 mv ./current/ ./previous/
@@ -15,6 +15,5 @@ find ./working -name '*.cvmfsoriginal' -exec cp --parents \{\} ./current/ \;
 mv ./current/working/ ./current/original
 find ./working -name "*.cvmfsoriginal" -type f -delete
 mv ./working/ ./current/zipped/
-ls ./current/zipped/
 tar -zcvf ./current/zipped.tar.gz ./current/zipped/ > ./current/tar.log
 
